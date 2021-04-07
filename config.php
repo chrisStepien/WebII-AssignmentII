5ec2f8f95452efe560c15722d97e91068f7f3080
Chris Stepien <Chris-t-fir@hotmail.com>
2021-04-07 17:00:17 -0600

    Hosting without database

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('DBHOST', 'localhost');
-define('DBNAME', 'assignment2');
-define('DBUSER', 'testuser');
-define('DBPASS', 'mypassword');
-define('DBCONNSTRING',"mysql:host=" . DBHOST . ";dbname=" . DBNAME . ";charset=utf8mb4;");
-?>
