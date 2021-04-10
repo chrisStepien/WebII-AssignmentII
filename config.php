9e7205033b49c9d42c6a82eac804f667e85ab27b
jramo193 <jramo193@mtroyal.ca>
2021-04-10 12:35:34 -0600

    Updated config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
-define('DBHOST', 'localhost');
-define('DBNAME', 'assignment2');
-define('DBUSER', 'testuser');
-define('DBPASS', 'mypassword');
+define('DBHOST', 'phtfaw4p6a970uc0.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
+define('DBNAME', 'gij9z58lxmjho289');
+define('DBUSER', 'twd2rqzft470q0ao');
+define('DBPASS', 's4m39lo2a53rosll');
 define('DBCONNSTRING',"mysql:host=" . DBHOST . ";dbname=" . DBNAME . ";charset=utf8mb4;");
 ?>
