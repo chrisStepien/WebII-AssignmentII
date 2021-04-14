b8213a189151fa42d7383c00f305dd1b1c119698
jramo193 <jramo193@mtroyal.ca>
2021-04-14 11:35:36 -0600

    Updated helper
    added number format

--- helper.php
+++ helper.php
@@ -3,11 +3,11 @@
 function outputTableRow($row) {
   echo "<tr>";
   echo "<td class='stockRow' id='stockDate'>" . $row['date'] . "</td>";
-  echo "<td class='stockRow' id='stockOpen'>" . $row['open'] . "</td>";
-  echo "<td class='stockRow' id='stockClose'>" . $row['close'] . "</td>";
-  echo "<td class='stockRow' id='stockLow'>" . $row['low'] . "</td>";
-  echo "<td class='stockRow' id='stockHigh'>" . $row['high'] . "</td>";
-  echo "<td class='stockRow' id='stockVolume'>" . $row['volume'] . "</td>";
+  echo "<td class='stockRow' id='stockOpen'>" . number_format($row['open'], 2, '.', '') . "</td>";
+  echo "<td class='stockRow' id='stockClose'>" . number_format($row['close'], 2, '.', '') . "</td>";
+  echo "<td class='stockRow' id='stockLow'>" . number_format($row['low'], 2, '.', '') . "</td>";
+  echo "<td class='stockRow' id='stockHigh'>" . number_format($row['high'], 2, '.', '') . "</td>";
+  echo "<td class='stockRow' id='stockVolume'>" . number_format($row['volume']) . "</td>";
   echo "</tr>";
 }
 
